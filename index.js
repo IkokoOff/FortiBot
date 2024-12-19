21418428c639bef2d5716727c96c7baec9b2e470
IkokoOff <159687221+IkokoOff@users.noreply.github.com>
2024-12-19 07:30:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ client.once("ready", async () => {
   await updateStatus();
 
   // Planifie l'exécution du script quotidien à 2h du matin tous les jours
-  cron.schedule('5 1 * * *', runDailyScript, {
+  cron.schedule('30 1 * * *', runDailyScript, {
     timezone: "Europe/Paris",
   });
 });
