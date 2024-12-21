fcbaafdadc583affe188932c075610a208e58b08
IkokoOff <159687221+IkokoOff@users.noreply.github.com>
2024-12-21 18:38:36 +0100

    Update ping.js

--- handlers/ping.js
+++ handlers/ping.js
@@ -2,7 +2,7 @@ module.exports.run = async (client, interaction) => {
   try {
     // Répondre à l'interaction avec la latence du bot
     const pingEmbed = {
-      color: "RANDOM",
+      color: "GREEN",
       title: "Pong! 🏓",
       description: `Latency: **${Date.now() - interaction.createdTimestamp}ms**\nAPI Latency: **${Math.round(client.ws.ping)}ms**`,
       footer: {
@@ -15,9 +15,22 @@ module.exports.run = async (client, interaction) => {
 
   } catch (error) {
     console.error("Error in /ping command:", error.message);
+
+    // Création d'un embed pour l'erreur
+    const errorEmbed = {
+      color: "RED",
+      title: "<a:denyanimated:1318915250627678269> Error",
+      description: `An error occurred while processing your request:\n\n**${error.message}**`,
+      footer: {
+        text: `Requested by ${interaction.user.tag}`,
+        icon_url: interaction.user.displayAvatarURL()
+      }
+    };
+
+    // Répondre avec un embed d'erreur
     interaction.reply({
-      content: "An error occurred while processing your request. Please try again later.",
-      ephemeral: true
+      embeds: [errorEmbed],
+      ephemeral: true // Rendre l'erreur visible uniquement pour l'utilisateur
     });
   }
 };
